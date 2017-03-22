656b95f09c20c0fa716da8532b545e40b3e1a0ea
Daniel D. Goodell <dgoodell@gmail.com>
2017-03-21 22:26:56 -0400

    entab

--- 1/21-entab.c
+++ 1/21-entab.c
@@ -1,20 +1,84 @@
 #include <stdio.h>
+#define MAXLINE 1000 /* maximum input line size*/
 
+void convertline(char line[], int len);
+int getline(char s[], int lim);
+void copy(char to[], char from[]);
 
-int main()
+int main(void)
 {
-	char c;
-	int i=0;
-	int tabspace=4 /* Width of tab stops */;
+	int len; /* Current Line Length */
+	char line[MAXLINE];
 
-	for(;(c=getchar()) != EOF; i++){
-		if (c == '\n'){
-			putchar('\n');
-			i=0;}
-		else if (c == '\t')
-			for(;printf(" "), i < tabspace; i++);
-		else putchar(c);
-		if (i == tabspace)
-			i=0;
+	while ((len = getline(line, MAXLINE)) > 0)
+	{
+		convertline(line[], len);
+		printf("%s", line);
 	}
+	return 0;
 }
+
+/* getline: read a line into s, return length */
+int getline(char s[], int lim)
+{
+	int c, i;
+
+	for(i=0; (c=getchar())!=EOF && c!='\n'; ++i){
+		if (i<lim-1)
+			s[i] = c;
+	}
+	if (i>lim-1){
+	if (c == '\n') {
+		s[lim-1] = ' ';
+		++i;
+	}
+	s[lim]= '\0';
+	}
+	else{
+	if (c == '\n') {
+		s[i] = ' ';
+		++i;
+	}
+		s[i]='\0';
+	}
+	return i;
+}
+
+/*reverseline: reverse a string of length len*/
+void convertline(char line[], int len)
+{
+	char buffer[len];
+	int tabspace=4;    /* Width of tab stops */
+	int spacelength=0; /*Accumulated spaces in current chunk of whitespace*/
+	int spacechars=0;  /*Number of chars */
+	int i = 0; /* Index for line */
+	int j = 0  /* Index for the buffer */
+
+	while((line[i] != '\0'; ){
+
+		if (line[i] != '\t' && line[i] != ' ' && spacelength == 0)
+			buffer[j] = line[i];
+		else if (spacelength > 0){
+			j=i-spacechars
+			while(spacelength > 0){
+				if(spacechars >= tabspace)
+				 printf
+			}
+		else if (line[i] == '\t' || line[i] == ' ' && spacelength == 0)
+	
+		}
+	i++
+	j++
+	}
+	copy(line, buffer);
+}
+
+/* copy: copy 'from' into 'to'; assume to is big enough */
+void copy(char to[], char from[])
+{
+	int i;
+	i = 0;	
+	while((to[i] = from[i]) != '\0')
+		++i;
+}
+
