8df679b312482a1187dddfc1bef0ab51eb1d94cd
Daniel D. Goodell <dgoodell@gmail.com>
2017-03-28 23:54:51 -0400

    added 23-decomment, almost works

---
+++ 1/23-decomment.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main(void){
+	int insidecomment = 0;
+	int insidequotes = 0;
+	int insidedoublequotes = 0;
+	char c;
+	
+	while((c=getchar()) != EOF){
+		if(c == '\\' && insidedoublequotes == 1){
+			putchar(c);
+			c = getchar();
+			putchar(c);}
+		else if(c == '\\' && insidequotes == 1){
+			putchar(c);
+			c = getchar();
+			putchar(c);}
+		else if((c == '\"') && (insidedoublequotes == 0) && (insidequotes == 0)){
+			putchar(c);
+			insidequotes=1;}
+		else if(c == '\'' && (insidequotes == 0) && (insidedoublequotes == 0)){
+			putchar(c);
+			insidequotes=1;}
+		else if(c == '\"' && insidedoublequotes == 1){
+			putchar(c);
+			insidedoublequotes=0;}
+		else if(c == '\'' && insidequotes== 1){
+			putchar(c);
+			insidequotes=0;}
+		else if(c == '/' && (insidequotes == 0) && (insidedoublequotes == 0)){
+			if((c=getchar()) == '/'){
+				insidecomment = 1;
+				while((c = getchar()) != '\n')
+					;
+				insidecomment = 0;
+				putchar('\n');}
+
+			else if(c == '*'){
+				insidecomment = 1;
+				while(insidecomment == 1){
+					while ((c=getchar()) != '*');
+					if((c=getchar()) == '/')
+						insidecomment = 0;
+				}
+			}
+			else putchar('/');
+		}
+		else
+			putchar(c);
+	}
+printf("\nComment' = %d", insidecomment);
+printf("\nDoubleQuotes = %d", insidedoublequotes);
+printf("\nQuotes = %d", insidequotes);
+}
+
+
+
+
