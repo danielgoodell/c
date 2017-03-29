16fe60e001937b68e9bbc8a497e5ed6d108be5fb
Daniel Goodell <dgoodell@gmail.com>
2017-03-29 14:08:06 -0400

    some better tests for 23-decomment, more work needed to pass them

--- 1/23-decomment.c
+++ 1/23-decomment.c
@@ -7,50 +7,51 @@ int main(void){
 	char c;
 	
 	while((c=getchar()) != EOF){
-		if(c == '\\' && insidedoublequotes == 1){
+		if((c == '\\') && (insidedoublequotes == 1)){
 			putchar(c);
 			c = getchar();
 			putchar(c);}
-		else if(c == '\\' && insidequotes == 1){
+		else if((c == '\\') && (insidequotes == 1)){
 			putchar(c);
 			c = getchar();
 			putchar(c);}
 		else if((c == '\"') && (insidedoublequotes == 0) && (insidequotes == 0)){
 			putchar(c);
 			insidequotes=1;}
-		else if(c == '\'' && (insidequotes == 0) && (insidedoublequotes == 0)){
+		else if((c == '\'') && (insidequotes == 0) && (insidedoublequotes == 0)){
 			putchar(c);
 			insidequotes=1;}
-		else if(c == '\"' && insidedoublequotes == 1){
+		else if((c == '\"') && (insidedoublequotes == 1)){
 			putchar(c);
 			insidedoublequotes=0;}
-		else if(c == '\'' && insidequotes== 1){
+		else if((c == '\'') && insidequotes== 1){
 			putchar(c);
 			insidequotes=0;}
-		else if(c == '/' && (insidequotes == 0) && (insidedoublequotes == 0)){
+		else if((c == '/') && (insidequotes == 0) && (insidedoublequotes == 0)){
 			if((c=getchar()) == '/'){
 				insidecomment = 1;
 				while((c = getchar()) != '\n')
 					;
 				insidecomment = 0;
 				putchar('\n');}
-
 			else if(c == '*'){
 				insidecomment = 1;
 				while(insidecomment == 1){
 					while ((c=getchar()) != '*');
 					if((c=getchar()) == '/')
-						insidecomment = 0;
-				}
-			}
-			else putchar('/');
+						insidecomment = 0;}}
+					else if (c == '*')
+						
+			else {
+				putchar('/');
+				putchar(c); }
 		}
 		else
 			putchar(c);
 	}
-printf("\nComment' = %d", insidecomment);
-printf("\nDoubleQuotes = %d", insidedoublequotes);
-printf("\nQuotes = %d", insidequotes);
+//printf("\nComment' = %d", insidecomment);
+//printf("\nDoubleQuotes = %d", insidedoublequotes);
+//printf("\nQuotes = %d", insidequotes);
 }
 
 
